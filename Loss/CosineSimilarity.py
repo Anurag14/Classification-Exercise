11a17c05786d48c2127336b325e2b4a71827239f
Anurag Singh <tellanurag95@gmail.com>
2019-07-09 16:09:27 +0530

    Update CosineSimilarity.py

--- Loss/CosineSimilarity.py
+++ Loss/CosineSimilarity.py
@@ -5,6 +5,10 @@ class MeanSquareLoss(object):
         self._minibatch = minibatch 
 
     def compute_loss(self, y_true, y_pred):
-        normalize_y_pred = tf.nn.l2_normalize(y_true,0)        
-        normalize_y_true = tf.nn.l2_normalize(y_pred,0)
-        return tf.reduce_sum( 1- tf.multiply( normalize_y_pred, normalize_y_true), 1, keep_dims=True )
+        if not self._minibatch:
+            normalize_y_pred = tf.nn.l2_normalize(y_true,0)        
+            normalize_y_true = tf.nn.l2_normalize(y_pred,0)
+            cos_similarity=tf.reduce_sum(tf.multiply(normalize_y_pred,normalize_y_true))
+            return 1-cos_similarity=tf.reduce_sum(tf.multiply(normalize_a,normalize_b))
+        else:
+            return tf.reduce_sum( 1- tf.multiply( normalize_y_pred, normalize_y_true), 1, keep_dims=True )
