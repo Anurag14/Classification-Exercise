d1e8b6b2fe98f6d996c5890eb237f2b4f34c9680
Anurag Singh <tellanurag95@gmail.com>
2019-07-09 17:55:52 +0530

    Update CosineSimilarity.py

--- Loss/CosineSimilarity.py
+++ Loss/CosineSimilarity.py
@@ -6,13 +6,13 @@ class CosineLoss(object):
 
     def compute_loss(self, y_true, y_pred):
         if not self._minibatch:
-            normalize_y_pred = tf.nn.l2_normalize(y_true,0)        
-            normalize_y_true = tf.nn.l2_normalize(y_pred,0)
+            normalize_y_pred = tf.nn.l2_normalize(y_pred,0)        
+            normalize_y_true = tf.nn.l2_normalize(y_true,0)
             cos_similarity=tf.reduce_sum(tf.multiply(normalize_y_pred,normalize_y_true))
             cosine_loss = 1-cos_similarity
         else:
-            normalize_a = tf.nn.l2_normalize(a,1)        
-            normalize_b = tf.nn.l2_normalize(b,1)
-            cos_similarity=tf.reduce_sum(tf.multiply(normalize_a,normalize_b),1)
+            normalize_y_pred = tf.nn.l2_normalize(y_pred,1)        
+            normalize_y_true = tf.nn.l2_normalize(y_true,1)
+            cos_similarity=tf.reduce_sum(tf.multiply(normalize_y_pred,normalize_y_true),1)
             cosine_loss = tf.reduce_sum(1-cos_similarity)
         return cosine_loss
