f47b37e1f94179bdbe1d188c338863b67b39e192
Anurag Singh <tellanurag95@gmail.com>
2019-07-09 10:37:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from keras.callbacks import ReduceLROnPlateau
 from keras.preprocessing.image import ImageDataGenerator
 from keras.regularizers import l2
 from keras import backend as K
-from keras.models import Model
+from keras.models import Model, load_model
 from keras.datasets import cifar10
 import numpy as np
 import os
@@ -27,26 +27,13 @@ num_classes = 10
 subtract_pixel_mean = True
 
 # Model parameter
-# ----------------------------------------------------------------------------
-#           |      | 200-epoch | Orig Paper| 200-epoch | Orig Paper| sec/epoch
-# Model     |  n   | ResNet v1 | ResNet v1 | ResNet v2 | ResNet v2 | GTX1080Ti
-#           |v1(v2)| %Accuracy | %Accuracy | %Accuracy | %Accuracy | v1 (v2)
-# ----------------------------------------------------------------------------
-# ResNet20  | 3 (2)| 92.16     | 91.25     | -----     | -----     | 35 (---)
-# ResNet32  | 5(NA)| 92.46     | 92.49     | NA        | NA        | 50 ( NA)
-# ResNet44  | 7(NA)| 92.50     | 92.83     | NA        | NA        | 70 ( NA)
-# ResNet56  | 9 (6)| 92.71     | 93.03     | 93.01     | NA        | 90 (100)
-# ResNet110 |18(12)| 92.65     | 93.39+-.16| 93.15     | 93.63     | 165(180)
-# ResNet164 |27(18)| -----     | 94.07     | -----     | 94.54     | ---(---)
-# ResNet1001| (111)| -----     | 92.39     | -----     | 95.08+-.14| ---(---)
-# ---------------------------------------------------------------------------
 n = 3
 
 # Model version
-# Orig paper: version = 1 (ResNet v1), Improved ResNet: version = 2 (ResNet v2)
+# version = 1 (ResNet v1), version = 2 (ResNet v2)
 version = 1
 
-# Computed depth from supplied model parameter n
+# Depth is computed from supplied model parameter n
 if version == 1:
     depth = n * 6 + 2
 elif version == 2:
@@ -82,9 +69,7 @@ y_test = keras.utils.to_categorical(y_test, num_classes)
 
 
 def lr_schedule(epoch):
-    """Learning Rate Schedule
-    Learning rate is scheduled to be reduced after 80, 120, 160, 180 epochs.
-    Called automatically every epoch as part of callbacks during training.
+    """Learning Rate Schedule: Learning rate is scheduled to be reduced after 80, 120, 160, 180 epochs.
     # Arguments
         epoch (int): The number of epochs
     # Returns
@@ -146,7 +131,7 @@ def resnet_layer(inputs,
     return x
 
 
-def resnet_v1(input_shape, depth, num_classes=10):
+def resnet_v1(input_shape, depth, num_classes=10, pre_trained=False, model_file_path=None):
     """ResNet Version 1 Model builder [a]
     Stacks of 2 x (3 x 3) Conv2D-BN-ReLU
     Last ReLU is after the shortcut connection.
@@ -158,21 +143,25 @@ def resnet_v1(input_shape, depth, num_classes=10):
     stage 0: 32x32, 16
     stage 1: 16x16, 32
     stage 2:  8x8,  64
-    The Number of parameters is approx the same as Table 6 of [a]:
-    ResNet20 0.27M
-    ResNet32 0.46M
-    ResNet44 0.66M
-    ResNet56 0.85M
-    ResNet110 1.7M
     # Arguments
         input_shape (tensor): shape of input image tensor
         depth (int): number of core convolutional layers
         num_classes (int): number of classes (CIFAR10 has 10)
+        pre_trained (bool): Toggle variable to specify if model is pre-trained
     # Returns
         model (Model): Keras model instance
     """
     if (depth - 2) % 6 != 0:
         raise ValueError('depth should be 6n+2 (eg 20, 32, 44 in [a])')
+    if pre_trained==True and model_file_path is not None:
+        pre_trained_model = Model.load_model(model_file_path)
+        outputs = Dense(num_classes,
+                    activation='softmax',
+                    kernel_initializer='he_normal')(pre_trained_model.layers[-1].output)
+
+        # Instantiate model.
+        model = Model(inputs=pre_trained_model.input, outputs=outputs)
+        return model
     # Start model definition.
     num_filters = 16
     num_res_blocks = int((depth - 2) / 6)
@@ -217,7 +206,7 @@ def resnet_v1(input_shape, depth, num_classes=10):
     return model
 
 
-def resnet_v2(input_shape, depth, num_classes=10):
+def resnet_v2(input_shape, depth, num_classes=10, model_file_path=None):
     """ResNet Version 2 Model builder [b]
     Stacks of (1 x 1)-(3 x 3)-(1 x 1) BN-ReLU-Conv2D or also known as
     bottleneck layer
@@ -241,6 +230,15 @@ def resnet_v2(input_shape, depth, num_classes=10):
     """
     if (depth - 2) % 9 != 0:
         raise ValueError('depth should be 9n+2 (eg 56 or 110 in [b])')
+    if pre_trained==True and model_file_path is not None:
+        pre_trained_model = Model.load_model(model_file_path)
+        outputs = Dense(num_classes,
+                    activation='softmax',
+                    kernel_initializer='he_normal')(pre_trained_model.layers[-1].output)
+
+        # Instantiate model.
+        model = Model(inputs=pre_trained_model.input, outputs=outputs)
+        return model
     # Start model definition.
     num_filters_in = 16
     num_res_blocks = int((depth - 2) / 9)
