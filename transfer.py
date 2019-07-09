64bfbff3e958db282afbb34400699a4c3a05f583
Anurag Singh <tellanurag95@gmail.com>
2019-07-09 12:19:13 +0530

    Update transfer.py

--- transfer.py
+++ transfer.py
@@ -1,59 +1,188 @@
-from keras.models import Sequential
-from keras.layers import Input,Flatten
-from keras.layers.convolutional import Convolution2D, MaxPooling2D
-from keras.layers.core import Dropout, Activation
-from keras.layers.pooling import GlobalAveragePooling2D
-from keras.models import Model
-in_img = Input(shape=(3, 32, 32))
+"""
+Trains a ResNet on the CIFAR100 dataset.
+"""
 
-def gen_model():
-    in_img = Input(shape=(3, 32, 32))
-    x = Convolution2D(12, 3, 3, subsample=(2, 2), border_mode='valid', name='conv1')(in_img)
-    x = Activation('relu', name='relu_conv1')(x)
-    x = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), name='pool1')(x)
-    x = Convolution2D(3, 1, 1, border_mode='valid', name='conv2')(x)
-    x = Activation('relu', name='relu_conv2')(x)
-    x = GlobalAveragePooling2D()(x)
-    o = Activation('softmax', name='loss')(x)
-    model = Model(input=in_img, output=[o])
-    return model
+from __future__ import print_function
+import keras
+from keras.optimizers import Adam
+from keras.callbacks import ModelCheckpoint, LearningRateScheduler
+from keras.callbacks import ReduceLROnPlateau
+from keras.preprocessing.image import ImageDataGenerator
+from keras.regularizers import l2
+from keras import backend as K
+from keras.datasets import cifar100
+from resnet import resnet_v1, resnet_v2
+import numpy as np
+import os
 
-#parent model
-model=gen_model()
-model.compile(loss="categorical_crossentropy", optimizer="adam")
+# Training parameters
+batch_size = 32  # orig paper trained all networks with batch_size=128
+epochs = 200
+data_augmentation = True
+num_classes = 20
+
+# Subtracting pixel mean improves accuracy
+subtract_pixel_mean = True
+
+# Model parameter
+n = 3
+
+# Model version
+# version = 1 (ResNet v1), version = 2 (ResNet v2)
+version = 1
+
+# Depth is computed from supplied model parameter n
+if version == 1:
+    depth = n * 6 + 2
+elif version == 2:
+    depth = n * 9 + 2
+
+# Model name, depth and version
+model_type = 'ResNet%dv%d' % (depth, version)
+
+# Load the CIFAR10 data.
+(x_train, y_train), (x_test, y_test) = cifar100.load_data()
+
+# Input image dimensions.
+input_shape = x_train.shape[1:]
+
+# Normalize data.
+x_train = x_train.astype('float32') / 255
+x_test = x_test.astype('float32') / 255
+
+# If subtract pixel mean is enabled
+if subtract_pixel_mean:
+    x_train_mean = np.mean(x_train, axis=0)
+    x_train -= x_train_mean
+    x_test -= x_train_mean
+
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+print('y_train shape:', y_train.shape)
+
+# Convert class vectors to binary class matrices.
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+
+def lr_schedule(epoch):
+    """Learning Rate Schedule: Learning rate is scheduled to be reduced after 80, 120, 160, 180 epochs.
+    # Arguments
+        epoch (int): The number of epochs
+    # Returns
+        lr (float32): learning rate
+    """
+    lr = 1e-3
+    if epoch > 180:
+        lr *= 0.5e-3
+    elif epoch > 160:
+        lr *= 1e-3
+    elif epoch > 120:
+        lr *= 1e-2
+    elif epoch > 80:
+        lr *= 1e-1
+    print('Learning rate: ', lr)
+    return lr
+model_file_path="<path to your pre trained model>"+".h5"
+if version == 2:
+    model = resnet_v2(input_shape=input_shape, depth=depth, model_file_path=model_file_path, pre_trained=True)
+else:
+    model = resnet_v1(input_shape=input_shape, depth=depth, model_file_path=model_file_path, pre_trained=True)
+
+model.compile(loss='categorical_crossentropy',
+              optimizer=Adam(lr=lr_schedule(0)),
+              metrics=['accuracy'])
 model.summary()
+print(model_type)
 
-#saving model weights
-model.save('model_weights.h5')
+# Prepare model model saving directory.
+save_dir = os.path.join(os.getcwd(), 'saved_models')
+model_name = 'cifar100_%s_model.{epoch:03d}.h5' % model_type
+if not os.path.isdir(save_dir):
+    os.makedirs(save_dir)
+filepath = os.path.join(save_dir, model_name)
 
-#loading weights to second model
-model2=gen_model()
-model2.compile(loss="categorical_crossentropy", optimizer="adam")
-model2.load_weights('model_weights.h5', by_name=True)
+# Prepare callbacks for model saving and for learning rate adjustment.
+checkpoint = ModelCheckpoint(filepath=filepath,
+                             monitor='val_acc',
+                             verbose=1,
+                             save_best_only=True)
 
-model2.layers.pop()
-model2.layers.pop()
-model2.summary()
+lr_scheduler = LearningRateScheduler(lr_schedule)
 
-#editing layers in the second model and saving as third model
-x = MaxPooling2D()(model2.layers[-1].output)
-o = Activation('sigmoid', name='loss')(x)
-model3 = Model(input=in_img, output=[o])
+lr_reducer = ReduceLROnPlateau(factor=np.sqrt(0.1),
+                               cooldown=0,
+                               patience=5,
+                               min_lr=0.5e-6)
 
-model.summary()
-model.layers.pop()
-model.layers.pop()
-model.summary()
+callbacks = [checkpoint, lr_reducer, lr_scheduler]
 
-x = BatchNormalization()(x)
-x = Activation('relu')(x)
-x = AveragePooling2D(pool_size=8)(x)
-y = Flatten()(x)
-outputs = Dense(num_classes,
-                activation='softmax',
-                kernel_initializer='he_normal')(y)
-x = MaxPooling2D()(model.layers[-1].output)
-o = Activation('sigmoid', name='loss')(x)
+# Run training, with or without data augmentation.
+if not data_augmentation:
+    print('Not using data augmentation.')
+    model.fit(x_train, y_train,
+              batch_size=batch_size,
+              epochs=epochs,
+              validation_data=(x_test, y_test),
+              shuffle=True,
+              callbacks=callbacks)
+else:
+    print('Using real-time data augmentation.')
+    # This will do preprocessing and realtime data augmentation:
+    datagen = ImageDataGenerator(
+        # set input mean to 0 over the dataset
+        featurewise_center=False,
+        # set each sample mean to 0
+        samplewise_center=False,
+        # divide inputs by std of dataset
+        featurewise_std_normalization=False,
+        # divide each input by its std
+        samplewise_std_normalization=False,
+        # apply ZCA whitening
+        zca_whitening=False,
+        # epsilon for ZCA whitening
+        zca_epsilon=1e-06,
+        # randomly rotate images in the range (deg 0 to 180)
+        rotation_range=0,
+        # randomly shift images horizontally
+        width_shift_range=0.1,
+        # randomly shift images vertically
+        height_shift_range=0.1,
+        # set range for random shear
+        shear_range=0.,
+        # set range for random zoom
+        zoom_range=0.,
+        # set range for random channel shifts
+        channel_shift_range=0.,
+        # set mode for filling points outside the input boundaries
+        fill_mode='nearest',
+        # value used for fill_mode = "constant"
+        cval=0.,
+        # randomly flip images
+        horizontal_flip=True,
+        # randomly flip images
+        vertical_flip=False,
+        # set rescaling factor (applied before any other transformation)
+        rescale=None,
+        # set function that will be applied on each input
+        preprocessing_function=None,
+        # image data format, either "channels_first" or "channels_last"
+        data_format=None,
+        # fraction of images reserved for validation (strictly between 0 and 1)
+        validation_split=0.0)
 
-model2 = Model(input=in_img, output=[o])
-model2.summary()
+    # Compute quantities required for featurewise normalization
+    # (std, mean, and principal components if ZCA whitening is applied).
+    datagen.fit(x_train)
+
+    # Fit the model on the batches generated by datagen.flow().
+    model.fit_generator(datagen.flow(x_train, y_train, batch_size=batch_size),
+                        validation_data=(x_test, y_test),
+                        epochs=epochs, verbose=1, workers=4,
+                        callbacks=callbacks)
+
+# Score trained model.
+scores = model.evaluate(x_test, y_test, verbose=1)
+print('Test loss:', scores[0])
+print('Test accuracy:', scores[1])
